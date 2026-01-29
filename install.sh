9cc452992d74e750ceefc0a8b0e7d3e0dc1aa179
prayudahlah <yudafihan@gmail.com>
2026-01-29 21:59:32 +0700

    add install.sh file

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# install.sh - Install dependencies for dotfiles
+
+# Arch Linux example
+sudo pacman -S niri kitty fish fisher neovim fastfetch \
+  wl-clipboard grim nodejs python python-pip \
+  ttf-nerd-fonts-symbols ttf-font-awesome
